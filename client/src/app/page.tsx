990ea33a3d8fefd5ff30091fc492bb99fd6b3ff7
Aditya Verma <adityaverma2528@outlook.com>
2024-11-06 10:11:31 +0530

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,44 +1,15 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-import { Socket } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
-let socket: Socket;
+let socket: Socket = io("http://localhost:4000", {
+    withCredentials: true,
+})
 
 export default function Page() {
-    const [message, setMessage] = useState("");
-    const [broadMsg, setBroadMsg] = useState(""); 
-
-    const handleClick = () => {
-        if (socket) {
-            socket.emit("message", message); 
-        }
-    };
-
-    useEffect(() => {
-        socket = io("http://localhost:4000", {
-            withCredentials: true,
-        })
-
-        socket.on("broadcastMsg", (data: string) => {
-            setBroadMsg(data); 
-        })
-    });
 
     return (
-        <div>
-            <input
-                type="text"
-                value={message}
-                onChange={(e) => {
-                    setMessage(e.target.value);
-                }}
-                className="p-5"
-            />
-            <button type="submit" onClick={handleClick}>send</button>
-
-            <p>{broadMsg}</p>
-        </div>
+        
     );
 }
